ecc46daa7a349bad684beb06eefa003da134e0d8
yian <crayon715@gmail.com>
2026-09-03 15:11:00 +0800

    t

--- nano5.sh
+++ nano5.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=vggt
-#SBATCH --partition=8gpus
-#SBATCH --time=2-00:00:00
-#SBATCH --account=MST114564
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=4
-#SBATCH --cpus-per-task=32
-#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=vggt		# 工作名稱
+#SBATCH --partition=dev				# 使用的 partition (請根據你的系統修改)
+#SBATCH --time=02:00:00				# 執行時間上限 (小時:分鐘:秒)
+#SBATCH --account=MST114564	####### 請記得換成您的計畫代碼 #######
+#SBATCH --nodes=1				# (-N) Maximum number of nodes to be allocated
+#SBATCH --gpus-per-node=4			# Gpus per node
+#SBATCH --cpus-per-task=12			# (-c) Number of cores per MPI task
+#SBATCH --ntasks-per-node=1			# Maximum number of tasks on each nodes
 
 # Email 通知
 #SBATCH --mail-type=BEGIN,END,FAIL
